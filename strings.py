a98f53d109cd266696b34eaa5c3f68848280986a
Yakumo Saki <yakumo@ziomatrix.org>
2023-02-24 00:51:02 +0900

    first commit

---
+++ strings.py
@@ -0,0 +1,8 @@
+class Strings:
+    APP_TITLE = "Work Tools"
+    MENU_START_POMO = "Start pomo"
+    MENU_STOP_POMO = "Stop pomo"
+    MENU_POMO_AUTO_LIGHT = "Set Light color on focus"
+    MENU_SET_LIGHT_RED = "Set Light Red"
+    MENU_SET_LIGHT_YELLOW = "Set Light Yellow"
+    MENU_PREFERENCES = "Preferences"
